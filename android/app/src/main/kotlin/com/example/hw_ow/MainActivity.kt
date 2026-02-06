28d31db389fdef07be9fbd4c652d5815e3e22454
Adnan Unal <a.unal677@gmail.com>
2026-02-06 13:28:12 +0100

    feat: initial commit

---
+++ android/app/src/main/kotlin/com/example/hw_ow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hw_ow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/hw_reader.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:hw_ow/sensors/mem.dart';
+
+// This is the entry point for our background worker
+void hardwareWorker(SendPort sendPort) async {
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    try {
+
+      final memInfo = await getMemoryStats();
+
+      // Send the data back as a Map
+      sendPort.send({
+       'memory': memInfo
+      });
+    } catch (e) {
+      sendPort.send({'error': e.toString()});
+    }
+  });
+}
+
